3d6f90d6930924979caa61899309a541ecae2ef5
Rodrigo Bonzerr S. Lopez <28951302+rodsafetyofficer@users.noreply.github.com>
2018-02-18 01:45:29 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<title>PHP CALCULATOR</title>
+<meta charset="UTF-8">
+</head>
+<body>
+  <form>
+    <input type="text" name="num1" placeholder="Number1">
+    <input type="text" name="num2" placeholder="Number2">
+    <select name="operator">
+      <option></option>
+    </select>
+    
+  </form>
+<p>The answer is:</p>
+<?php
+
+?>
+</body>
+</html>
